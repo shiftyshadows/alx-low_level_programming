244f70866492bb42f4d24f49fa8c0cf54ec50879
Paul A <purple.quasar@proton.me>
2023-02-16 22:20:46 +0300

    Hello World: Additional Tasks

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -17,9 +17,9 @@ int main(void)
 
 /* Displays the string inside quotations */
 	printf("Size of a char: %lu byte(s)\n", sizeof(acharacter));
-	printf("Size of a int: %lu byte(s)\n", sizeof(ainteger));
+	printf("Size of an int: %lu byte(s)\n", sizeof(ainteger));
 	printf("Size of a long int: %lu byte(s)\n", sizeof(along));
-	printf("Size of a long long: %lu byte(s)\n", sizeof(alonglong));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(alonglong));
 	printf("Size of a float: %lu byte(s)\n", sizeof(afloat));
 
 	return (0);
