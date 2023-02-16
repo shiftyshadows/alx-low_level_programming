5f0b4741d18d4833b7c49b4ed95820c6a32414f4
Paul A <purple.quasar@proton.me>
2023-02-16 22:11:12 +0300

    Hello World: Additional Tasks

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,8 +9,7 @@
 int main(void)
 {
 	/* Displays given string to standard error */
-	fprintf(stderr, "and that piece of art is useful\"
-		- Dora Korpar, 2015-10-19\n");
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 
 	return (1);
 }
