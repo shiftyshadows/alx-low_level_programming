10b74c0224539547dc98bdc7610ba9ae8f0e0d03
Paul A <purple.quasar@proton.me>
2023-02-21 05:59:13 +0300

    Variables; If..Else..While

---
+++ 0x01-variables_if_else_while/102-print_comb.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ *main - Entry Point
+ *Return: Always(0) - Success.
+ */
+
+int main(void)
+{
+/*Declaration of variables*/
+	int i, j;
+
+/*Code Logic*/
+	for (i = 0; i <= 99; i++)
+	{
+		for (j = i; j <= 99; j++)
+		{
+			if (j != i)
+			{
+				putchar(i / 10 + 48);
+				putchar(i % 10 + 48);
+				putchar(' ');
+				putchar(j / 10 + 48);
+				putchar(j % 10 + 48);
+
+				if (i * 100 + j != 9899)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
