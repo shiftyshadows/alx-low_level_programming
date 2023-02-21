147b062fbbd6b35f47db49ad0955ae418d9807de
Paul A <purple.quasar@proton.me>
2023-02-21 05:55:27 +0300

    Variables; If..Else..While

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,21 +1,46 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
- *main - Entry Point (Prints all possible different combinations of 3 digits)
+ *main - Entry Point
  *Return: Always(0) - Success.
  */
 
 
 int main(void)
 {
-/*Declaration of varialbles*/
-	char h = '0', t = '1', u = '2';
+/*Declaration of Variables*/
+	int c;
+	int d;
+	int e = 0;
 
 /*Code Logic*/
-	while ((h <= '7') || (t <= '8') || (u <= '9'))
+	while (e < 10)
 	{
-		printf("%c%c%c, ", h, t, u);
-		u != '9' ? ++u : (t != '8' ? (++t, u = t + 1) : (++h, t = h + 1, u = t + 1));
+		d = 0;
+		while (d < 10)
+		{
+			c = 0;
+			while (c < 10)
+			{
+				if (c != d && d != e && e < d && d < c)
+				{
+					putchar('0' + e);
+					putchar('0' + d);
+					putchar('0' + c);
+
+					if (c + d + e != 9 + 8 + 7)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+
+				c++;
+			}
+			d++;
+		}
+		e++;
 	}
 	putchar('\n');
 	return (0);
