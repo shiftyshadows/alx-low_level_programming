3bb3034cbc88b62c137227055f4bf4bca0c4cc6d
Paul A <purple.quasar@proton.me>
2023-02-22 05:35:33 +0300

    Functions; Nested Loops

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,14 +1,27 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
- *main - Entry Point
- *Return: Always(0) - Success.
+ * print_sign - prints the sign of a number
+ *@n: number being tested
+ * Return: Always 0.
  */
 
-int main(void)
+int print_sign(int n)
 {
-/*Declaration of variables*/
-
-/*Code Logic*/
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
 	return (0);
 }
