b5ecbc5c4517e5f42aa76b73e45cc5e0f2a9ed18
Paul A <purple.quasar@proton.me>
2023-02-22 21:55:43 +0300

    main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,9 +2,9 @@
 #define MAIN_H
 
 /*
- * File: main.h
+ *File: main.h
  *
- * Desc: Header file containing declartions for all function
+ *Desc: Header file containing declarations for all function
  */
 
 int _putchar(char c);
@@ -16,7 +16,7 @@ int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
-void time_teble(void);
+void time_table(void);
 int add(int, int);
 void print_to_98(int n);
 void print_time_table(int n);
