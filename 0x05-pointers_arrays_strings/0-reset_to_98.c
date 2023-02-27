cd70b7d955f9ce8546886486fb29d60515601eaa
Shiftyshadows <purple.quasar@proton.me>
2023-02-27 21:06:54 +0300

    98 Battery st

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - takes a pointer to an int
+ * as parameter and updates the value it
+ * points to to 98
+ *
+ * @n: number to modify
+ */
+
+void reset_to_98(int *n)
+{
+/*Declaration of variables*/
+	*n = 98
+
+}
