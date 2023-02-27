8f02ae2c290a3b8dea096eca6e531709039482bd
Shiftyshadows <purple.quasar@proton.me>
2023-02-28 01:17:32 +0300

    Revert "Half the lies they tell about me aren't true"
    This reverts commit d0856f3af2905e0b40baca1671a2e7a5eb5ff377.

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -14,13 +14,9 @@ void puts2(char *str)
 	int i;
 
 /* Iterate over the string, printing every other character */
-	while (str[i] != '\0')
+	for (i = 0; str[i] != '\0'; i += 2)
 	{
-		if (i % 2 == 0)
-		{
-			_putchar(str[i]);
-		}
-		i++;
+		_putchar(str[i]);
 	}
 
 /*Print a new line*/
