da8e49c09fbcb9cf7665bc1858368c2679535d7e
Shiftyshadows <purple.quasar@proton.me>
2023-02-28 02:12:28 +0300

    Arrays are not pointers

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -2,9 +2,9 @@
 
 /**
  * *_strcpy - copies the string pointed to by src, including \0.
+ *
  *@src: pointer.
  *@dest: pointer.
- *Return: the pointer to dest.
  */
 
 char *_strcpy(char *dest, char *src)
