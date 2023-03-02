44110f92a9f44e2efa2063069e6c85c01351596f
Shiftyshadows <purple.quasar@proton.me>
2023-03-02 20:36:13 +0300

    Noise is a buffer, more effective than cubicles or booth walls

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,21 +1,56 @@
 #include "main.h"
+#include <stdio.h>
+#include <ctype.h>
 
 /**
- * swap_int - swaps the values of two integers
+ * print_buffer - swaps the values of two integers
  *
- * @a: number being swapped
+ * @size: number being swapped
  * @b: number being swapped
  *
  */
 
-void swap_int(int *a, int *b)
+
+void print_buffer(char *b, int size)
 {
 /*Declaration of variables*/
-	int c;
+	int i, j;
 
-/*Code Logic*/
-	c = 0;
-	c = *a;
-	*a = *b;
-	*b = c;
+/*Code Statements*/
+	for (i = 0; i < size; i += 10)
+	{
+		printf("%08x: ", i);
+		for (j = 0; j < 10; j++)
+		{
+			if (i + j < size)
+			{
+				printf("%02x ", (unsigned char)b[i + j]);
+			}
+			else
+			{
+				printf("   ");
+			}
+		}
+		printf(" ");
+		for (j = 0; j < 10; j++)
+		{
+			if (i + j < size)
+			{
+				if (isprint(b[i + j]))
+				{
+					printf("%c", b[i + j]);
+				}
+				else
+				{
+					printf(".");
+				}
+			}
+		}
+		printf("\n");
+	}
+	if (size <= 0)
+	{
+		printf("\n");
+	}
 }
+
