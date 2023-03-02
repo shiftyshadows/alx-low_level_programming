07e1c27f774e698bb906d7d3bf95d1ec873bab2e
Shiftyshadows <purple.quasar@proton.me>
2023-03-02 18:23:44 +0300

    Expect the best. Prepare for the worst. Capitalize on what comes

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,21 +1,47 @@
 #include "main.h"
 
 /**
- * swap_int - swaps the values of two integers
+ * cap_string - function that capitalizes all
+ * words of a string
  *
- * @a: number being swapped
- * @b: number being swapped
+ * @str: string to be capitalized
+ * Return: Capitalized string
  *
  */
 
-void swap_int(int *a, int *b)
+
+char *cap_string(char *str)
 {
 /*Declaration of variables*/
-	int c;
+int i;
+
+/*Flag to track if the previous character is a separator*/
+int sep = 1;
 
 /*Code Logic*/
-	c = 0;
-	c = *a;
-	*a = *b;
-	*b = c;
+/*Capitalize the first letter of the string*/
+	if (str[0] >= 'a' && str[0] <= 'z')
+	{
+		str[0] -= 32;
+	}
+	for (i = 1; str[i] != '\0'; i++)
+	{
+/*Capitalize the first letter of each word*/
+		if (sep && str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] -= 32;
+		}
+/* Convert any other uppercase letters to lowercase*/
+		else if (str[i] >= 'A' && str[i] <= 'Z')
+		{
+			str[i] += 32;
+		}
+/* Set the flag to track separators*/
+		sep =	(str[i] == ' ' || str[i] == '\t' || str[i] == '\n' ||
+			str[i] == ',' || str[i] == ';' || str[i] == '.' ||
+			str[i] == '!' || str[i] == '?' || str[i] == '"' ||
+			str[i] == '(' || str[i] == ')' || str[i] == '{' ||
+			str[i] == '}');
+	}
+	return (str);
 }
