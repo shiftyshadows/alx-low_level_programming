e269cb805c3b3da102c66b8d198c905e95042abc
Shiftyshadows <purple.quasar@proton.me>
2023-03-03 08:40:13 +0300

    isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,23 +1,23 @@
 #include "main.h"
 
 /**
- * _isupper - swaps the values of two integers
+ * _isupper - function that checks for uppercase character
  *
- * @a: number being swapped
- * @b: number being swapped
+ * @c: letter to be checked
  *
+ * Return: 0 (if not uppercase) or 1 (if uppercase)
  */
 
 int _isupper(int c)
 {
-	if(c >= 'A' && c <= 'Z')
+	if (c >= 'A' && c <= 'Z')
 	{
 	/*check if c is within the range of uppercase characters*/
-		return 1; /* return 1 if c is uppercase */
+		return (1); /* return 1 if c is uppercase */
 	}
 	else
 	{
-		return 0; /* return 0 if c is not uppercase */
+		return (0); /* return 0 if c is not uppercase */
 	}
 }
 
