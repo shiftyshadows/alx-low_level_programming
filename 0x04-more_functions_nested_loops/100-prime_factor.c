34f54aec0b1312ba7440c6cf04fdbddd97b10886
Shiftyshadows <purple.quasar@proton.me>
2023-03-03 10:10:48 +0300

    The problem of distinguishing prime numbers from composite numbers and of resolving the latter into their prime factors is known to be one of the most important and useful in arithmetic

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,21 +1,44 @@
-#include "main.h"
+#include <stdio.h>
+#include <math.h>
 
 /**
- * swap_int - swaps the values of two integers
+ * main - program that finds and prints the
+ * largest prime factor of the number 612852475143
  *
- * @a: number being swapped
- * @b: number being swapped
- *
+ * Return: Always 0 (Success)
  */
 
-void swap_int(int *a, int *b)
+
+int main(void)
 {
 /*Declaration of variables*/
-        int c;
+	long n = 612852475143;
+	long factor = 2;
+	long last_factor = 1;
 
-/*Code Logic*/
-        c = 0;
-        c = *a;
-        *a = *b;
-        *b = c;
+/*Code Statements*/
+	while (n > 1)
+	{
+		if (n % factor == 0)
+		{
+			last_factor = factor;
+			n /= factor;
+			while (n % factor == 0)
+			{
+				n /= factor;
+			}
+		}
+		factor++;
+		if (factor * factor > n)
+		{
+			if (n > 1)
+			{
+				last_factor = n;
+			}
+			break;
+		}
+	}
+	printf("%ld\n", last_factor);
+	return (0);
 }
+
