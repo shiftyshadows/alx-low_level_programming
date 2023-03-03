e7789f6b6b450aa0f1b89299ab3db5f4442c2a38
Shiftyshadows <purple.quasar@proton.me>
2023-03-03 07:48:47 +0300

    Software is eating the World

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,13 +1,40 @@
 #include <stdio.h>
-
 /**
- *main - Entry Point
- *Return: Always(0) - Success.
+ * main - prints the sum of all the multiples of 3 or 5 below 1024
+ * @void: void
+ * Return: 0 Success
  */
+
+
 
 int main(void)
 {
 /*Declaration of variables*/
+	int i, j;
 
-/*Code Logic*/
+/*Code Statements*/
+	for (i = 0; i <= 99; i++)
+	{
+		for (j = i; j <= 99; j++)
+		{
+			if (j != i)
+			/*Don't print comma and space after the last combination*/
+			{
+				putchar(i / 10 + 48);
+				putchar(i % 10 + 48);
+				putchar(' ');
+				putchar(j / 10 + 48);
+				putchar(j % 10 + 48);
+
+				if (i * 100 + j != 9899)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
 }
+
