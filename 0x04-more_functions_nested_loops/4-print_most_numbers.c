7c8eaebb5d1bd1d2219feaaf99103d53f1727dea
Shiftyshadows <purple.quasar@proton.me>
2023-03-03 09:08:05 +0300

    I believe in numbers and signs

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,21 +1,24 @@
 #include "main.h"
 
 /**
- * swap_int - swaps the values of two integers
- *
- * @a: number being swapped
- * @b: number being swapped
+ * print_most_numbers - function that prints the numbers,
+ * from 0 to 9, followed by a new line.
  *
  */
 
-void swap_int(int *a, int *b)
+void print_most_numbers(void)
 {
 /*Declaration of variables*/
-        int c;
+	int i;
 
 /*Code Statements*/
-        c = 0;
-        c = *a;
-        *a = *b;
-        *b = c;
+	for (i = 0; i < 10; i++)
+	{
+		if (i != 2 && i != 4)
+		{
+			_putchar(i + '0');
+		}
+	}
+	_putchar('\n');
 }
+
