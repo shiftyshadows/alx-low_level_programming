d2399f7077596aaaebb8ab254632ab3b979f8666
Shiftyshadows <purple.quasar@proton.me>
2023-03-09 10:36:29 +0300

    Dreams feel real while we're in them. It's only when we wake up that we realize something was actually strange

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -12,11 +12,11 @@ int _strlen_recursion(char *s)
 	/*Code Statements*/
 	if (*s == '\0')
 	{
-	return 0;
+	return (0);
 	}
 	else
 	{
-	return 1 + _strlen_recursion(s + 1);
+	return (1 + _strlen_recursion(s + 1));
 	}
 }
 
