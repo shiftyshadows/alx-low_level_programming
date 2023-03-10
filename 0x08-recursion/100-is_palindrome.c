28fa36fac3bcc32f5130acb3b8f9f44eda4975ba
Shiftyshadows <purple.quasar@proton.me>
2023-03-10 03:04:01 +0300

    They say we only use a fraction of our brain's true potential. Now that's when we're awake. When we're asleep, we can do almost anything

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,21 +1,50 @@
 #include "main.h"
 
 /**
- * swap_int - swaps the values of two integers
- *
- * @a: number being swapped
- * @b: number being swapped
- *
+ * _strlen_recursion - size
+ * @s: pointer to string params
+ * Return: recursion
  */
 
-void swap_int(int *a, int *b)
+int _strlen_recursion(char *s)
 {
-/*Declaration of variables*/
-	int c;
+	if (!*s)
+	{
+		return (0);
+	}
+	return (1 + _strlen_recursion(++s));
+}
 
-/*Code Logic*/
-	c = 0;
-	c = *a;
-	*a = *b;
-	*b = c;
+/**
+ * p1 - palindrome
+ * @s: pointer to string
+ * @l: position
+ * Return: boolena
+ */
+
+int p1(char *s, int l)
+{
+	if (l < 1)
+	{
+		return (1);
+	}
+
+	if (*s == *(s + l))
+	{
+		return (p1(s + 1, l - 2));
+	}
+	return (0);
+}
+
+/**
+ * is_palindrome - palindrome
+ * @s: pointer to string
+ * Return: recursion
+ */
+
+int is_palindrome(char *s)
+{
+	int len = _strlen_recursion(s);
+
+	return (p1(s, len - 1));
 }
