c006bdfed70c62d1d4f34a1983189ef916f33e5a
Shiftyshadows <purple.quasar@proton.me>
2023-03-14 22:39:00 +0300

    2. He who is not courageous enough to take risks will accomplish nothing in life

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,29 +1,47 @@
 #include "main.h"
+#include <stdlib.h>
+#include <string.h>
 
 /**
- * _strncpy - function that copies a string
+ * str_concat -  function that concatenates two strings.
  *
- * @dest: destination string
- * @src: source string
- * @n: number
+ * @s1:	pointer of type char containing string
+ *      to be concatenated
+ * @s2:	pointer of type char containing string
+ *	to be concatenated
  *
  * Return: Destination string
  */
 
-char *_strncpy(char *dest, char *src, int n)
+char *str_concat(char *s1, char *s2)
 {
-/*Declaration of variables*/
-	int i;
+	/*Declaration of variables*/
+	char *concat_str;
 
-/*Code Logic*/
-	for (i = 0; i < n && src[i] != '\0'; i++)
+
+	/* Code Statements */
+	/* Treat NULL strings as empty strings */
+	if (s1 == NULL)
 	{
-		dest[i] = src[i];
+		s1 = "";
 	}
-	for ( ; i < n; i++)
+	if (s2 == NULL)
 	{
-		dest[i] = '\0';
+		s2 = "";
 	}
-	return (dest);
+
+	/* Allocate memory for the concatenated string */
+	concat_str = malloc(strlen(s1) + strlen(s2) + 1);
+	if (concat_str == NULL)
+	{
+		return (NULL);
+	}
+
+	/* Copy the contents of s1 and s2 to the concatenated string */
+	strcpy(concat_str, s1);
+	strcat(concat_str, s2);
+
+	return (concat_str);
 }
+
 
