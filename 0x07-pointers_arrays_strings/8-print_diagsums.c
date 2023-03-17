d462969dc407e192d2f9882d556299f08dde8d0c
Shiftyshadows <purple.quasar@proton.me>
2023-03-17 10:11:24 +0300

    8.The line of life is a ragged diagonal between duty and desire

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,21 +1,27 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * swap_int - swaps the values of two integers
+ * print_diagsums - swaps the values of two integers
  *
  * @a: number being swapped
- * @b: number being swapped
+ * @size: number being swapped
  *
  */
 
-void swap_int(int *a, int *b)
-{
-/*Declaration of variables*/
-	int c;
 
-/*Code Logic*/
-	c = 0;
-	c = *a;
-	*a = *b;
-	*b = c;
+void print_diagsums(int *a, int size)
+{
+/* Declaration of Variables */
+	int sum1 = 0, sum2 = 0, i;
+
+/* Code Statements */
+	for (i = 0; i < size; i++)
+	{
+		sum1 += *(a + i * size + i);
+		sum2 += *(a + i * size + size - i - 1);
+	}
+	printf("Sum of main diagonal: %d\n", sum1);
+	printf("Sum of secondary diagonal: %d\n", sum2);
 }
+
