8c0e527265df72077cdc8543c0a3f1817fc9294d
Shiftyshadows <purple.quasar@proton.me>
2023-04-04 05:48:53 +0300

    11. Reverse list

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -13,18 +13,18 @@ listint_t *reverse_listint(listint_t **head)
 {
 /* Declaration of Variables */
 	listint_t *prev = NULL;
-	listint_t *current = *head;
 	listint_t *next = NULL;
 
 /* Code Statements */
-	while (current != NULL)
+	while (*head)
 	{
-		next = current->next;
-		current->next = prev;
-		prev = current;
-		current = next;
+		next = (*head)->next;
+		(*head)->next = prev;
+		prev = *head;
+		*head = next;
 	}
 
 	*head = prev;
+
 	return (*head);
 }
