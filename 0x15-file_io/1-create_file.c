95b3f1ba874c7e0d2ed6245372705f4686e1bfeb
Shiftyshadows <purple.quasar@proton.me>
2023-04-24 20:04:41 +0300

    1. Under the snow

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,21 +1,45 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
 
 /**
- * swap_int - swaps the values of two integers
+ * swap_int - Function that creates a file.
  *
- * @a: number being swapped
- * @b: number being swapped
+ * @filename: name of file to be created.
+ * @text_content: Output data to file.
  *
+ * Return: Integer file descriptor
  */
 
-void swap_int(int *a, int *b)
+
+int create_file(const char *filename, char *text_content)
 {
 /* Declaration of variables */
-	int c;
+	int fd;
 
 /* Code Statements */
-	c = 0;
-	c = *a;
-	*a = *b;
-	*b = c;
+	if (!filename)
+	{
+		return (-1);
+	}
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (fd == -1)
+	{
+		return (-1);
+	}
+
+	if (text_content)
+	{
+		ssize_t num_written = write(fd, text_content, strlen(text_content));
+		if (num_written == -1)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+	close(fd);
+	return (1);
 }
+
