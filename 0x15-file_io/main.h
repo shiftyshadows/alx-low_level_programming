7b4f817303410ecff156803fb535167d9ac024bd
Shiftyshadows <purple.quasar@proton.me>
2023-04-24 19:50:19 +0300

    0. Tread lightly, she is near

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <sys/types.h>
+#include <sys/types.h>
+
+/*
+ *File: main.h
+ *
+ *Desc: Header file containing declarations for all function
+ */
+
+int _putchar(char c);
+ssize_t read_textfile(const char *filename, size_t letters);
+
+
+#endif
