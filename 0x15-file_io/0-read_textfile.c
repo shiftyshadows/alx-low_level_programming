ce0ebd6190e8ef1c5911f1acfcc8bc01184a13ad
Shiftyshadows <purple.quasar@proton.me>
2023-04-24 19:57:21 +0300

    0. Tread lightly, she is near

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -22,26 +22,21 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	char *buf;
 	ssize_t bytes_read, bytes_written;
 
-
 /* Code Statements */
-
 	if (filename == NULL)
 	{
 		return (0);
 	}
-
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 	{
 		return (0);
 	}
-
 	buf = (char *) malloc((letters + 1) * sizeof(char));
 	if (buf == NULL)
 	{
 		return (0);
 	}
-
 	bytes_read = read(fd, buf, letters);
 	if (bytes_read == -1)
 	{
@@ -49,9 +44,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		close(fd);
 		return (0);
 	}
-
 	buf[bytes_read] = '\0'; /* add null terminator to the end of the buffer */
-
 	bytes_written = write(STDOUT_FILENO, buf, bytes_read);
 	if (bytes_written == -1 || bytes_written != bytes_read)
 	{
@@ -59,10 +52,8 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		close(fd);
 		return (0);
 	}
-
 	free(buf);
 	close(fd);
-
 	return (bytes_written);
 }
 
