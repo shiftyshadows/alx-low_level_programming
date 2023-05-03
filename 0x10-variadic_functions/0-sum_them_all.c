4357d196c7959fbfe5a94a288acf4c1ba56dea24
Shiftyshadows <purple.quasar@proton.me>
2023-05-04 01:49:07 +0300

    0. Beauty is variable, ugliness is constant

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,6 +1,4 @@
-#include <stdarg.h>
-#include <stdio.h>
-#include "main.h"
+#include "variadic_functions.h"
 
 /**
  * sum_them_all -
---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,17 @@
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
+
+#include <stdio.h>
+#include <stdarg.h>
+#include <unistd.h>
+
+/*
+ *File: variadic_functions.h
+ *
+ *Desc: Header file containing declarations for all function
+ */
+
+int sum_them_all(const unsigned int n, ...);
+int _putchar(char c);
+
+#endif /* variadic_functions.h  */
