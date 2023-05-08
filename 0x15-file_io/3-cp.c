c7eb183aafb6bcf9210a4ba28fad34047df1f376
Shiftyshadows <purple.quasar@proton.me>
2023-05-08 13:43:16 +0300

    2. Speak gently, she can hear

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+#define BUF_SIZE 1024
+
+/**
+ * main- program that appends text at the end of file.
+ *
+ * @argc: Number of arguments passed to main progam
+ * @argv: character pointer to character strings passed to
+ * main program
+ *
+ * Return: integer.
+ */
+
+int main(int argc, char *argv[])
+{
+	int fd_from, fd_to;
+	ssize_t nread;
+	char buf[BUF_SIZE];
+	mode_t mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: %s file_from file_to\n", argv[0]);
+		exit(97);
+	}
+	fd_from = open(argv[1], O_RDONLY);
+	if (fd_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, mode);
+	if (fd_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+	while ((nread = read(fd_from, buf, BUF_SIZE)) > 0)
+	{
+		if (write(fd_to, buf, nread) != nread)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			exit(99);
+		}
+	}
+	if (nread == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	if (close(fd_from) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
+		exit(100);
+	}
+	if (close(fd_to) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
+		exit(100);
+	}
+	return (0);
+}
