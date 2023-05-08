f090f78f372cf61de16150a93e548ac760928d80
Shiftyshadows <purple.quasar@proton.me>
2023-05-08 13:41:33 +0300

    1. Under the snow

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,47 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+
+/**
+ * create_file - Function that creates a file.
+ *
+ * @filename: name of file to be created.
+ * @text_content: Output data to file.
+ *
+ * Return: Integer file descriptor
+ */
+
+
+int create_file(const char *filename, char *text_content)
+{
+/* Declaration of variables */
+	int fd;
+	ssize_t num_written;
+
+/* Code Statements */
+	if (!filename)
+	{
+		return (-1);
+	}
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (fd == -1)
+	{
+		return (-1);
+	}
+
+	if (text_content)
+	{
+		num_written = write(fd, text_content, strlen(text_content));
+		if (num_written == -1)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+	close(fd);
+	return (1);
+}
+
