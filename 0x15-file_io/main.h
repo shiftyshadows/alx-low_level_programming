2fb81d75b12d8259e4c2f4c8f802617baa4586a0
Shiftyshadows <purple.quasar@proton.me>
2023-05-09 11:38:26 +0300

    main header correction

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,33 +1,34 @@
-#include <sys/types.h>
-
 #ifndef MAIN_H
 #define MAIN_H
 
-/*
- *File: main.h
- *
- *Desc: Header file containing declarations for all function
+#include <sys/types.h>
+
+/**
+ * typedef struct
+ * {
+ *       unsigned char e_ident[EI_NIDENT];
+ *       uint16_t e_type;
+ *       uint16_t e_machine;
+ *       uint32_t e_version;
+ *       uint64_t e_entry;
+ *       uint64_t e_phoff;
+ *       uint64_t e_shoff;
+ *       uint32_t e_flags;
+ *       uint16_t e_ehsize;
+ *       uint16_t e_phentsize;
+ *       uint16_t e_phnum;
+ *       uint16_t e_shentsize;
+ *       uint16_t e_shnum;
+ *       uint16_t e_shstrndx;
+ * } Elf64_Ehdr;
  */
 
+/**
+ * File: main.h
+ *
+ * Desc: Header file containing declarations for all function
+ */
 
-/*typedef struct
-{
-        unsigned char e_ident[EI_NIDENT];
-        uint16_t e_type;
-        uint16_t e_machine;
-        uint32_t e_version;
-        uint64_t e_entry;
-        uint64_t e_phoff;
-        uint64_t e_shoff;
-        uint32_t e_flags;
-        uint16_t e_ehsize;
-        uint16_t e_phentsize;
-        uint16_t e_phnum;
-        uint16_t e_shentsize;
-        uint16_t e_shnum;
-        uint16_t e_shstrndx;
-} Elf64_Ehdr;
-*/
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
