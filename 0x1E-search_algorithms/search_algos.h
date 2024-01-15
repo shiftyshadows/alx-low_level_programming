a5f49575d21d40afabb73af457de594c963b0915
Shiftyshadows <purple.quasar@proton.me>
2024-01-15 23:30:49 +0300

    7. Jump search

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,6 +1,8 @@
 #ifndef SEARCH_ALGOS_H
 #define SEARCH_ALGOS_H
 
+#include <stddef.h>
+#include <stdio.h>
 /*
  *File: search_algos.h
  *
@@ -9,5 +11,6 @@
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
+int jump_search(int *array, size_t size, int value);
 
 #endif
